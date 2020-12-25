d538004dba6084b652a017f99eed10af8aee92ac
Cem Acar <cemacarmail@yahoo.com>
2020-12-25 11:18:42 +0300

    CSS & Js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,25 +22,3 @@ btn.on('click', function(e) {
   }, '1300');
 });
 
-/** Navigation Scroll**/
- var nav_sections = $('section');
-  var main_nav = $('.navbar');
-
-  $(window).on('scroll', function() {
-    var cur_pos = $(this).scrollTop() + 200;
-
-    nav_sections.each(function() {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
-        }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".navbar-nav ul:first li:first").addClass('active');
-      }
-    });
-  });
