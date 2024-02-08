604f902139580f7d7ce02dd39870e25ea56bc66a
Cem Acar <cemacar@linxroom.com>
2024-02-08 15:26:04 +0300

    new template tools added

--- assets/js/template.js
+++ assets/js/template.js
@@ -38,5 +38,113 @@ export const templateList = [
         desc: "",
         link: "https://carrd.co/"
     
+    },
+    {
+        id: 6,
+        category: "Template", 
+        title: "Free CSS",
+        desc: "",
+        link: "https://www.free-css.com/"
+    
+    },
+    {
+        id: 7,
+        category: "Template", 
+        title: "FREEHTML5.CO",
+        desc: "",
+        link: "https://freehtml5.co/"
+    
+    },
+    {
+        id: 8,
+        category: "Template",
+        title: "graygrids",
+        desc: "",
+        link: "https://graygrids.com/"
+    },
+    {
+        id: 9,
+        category: "Template",
+        title: "html5-templates",
+        desc: "",
+        link: "https://html5-templates.com/"
+    },
+    {
+        id: 10, 
+        category: "Templates", 
+        title: "HTML5 UP",
+        desc: "",
+        link: "https://html5up.net/"
+    
+    },
+    {
+        id: 11, 
+        category: "Template",
+        title: "Mobirise",
+        desc: "",
+        link: "https://mobirise.com/"
+    
+    },
+    {
+        id: 12, 
+        category: "Template", 
+        title: "nicepage",
+        desc: "",
+        link: "https://nicepage.com/"
+    
+    },
+    {
+        id: 13, 
+        category: "Template", 
+        title: "One Page Love",
+        desc: "",
+        link: "https://onepagelove.com/"
+    
+    },
+    {
+        id: 14, 
+        category: "Template", 
+        title: "TEMPLATED",
+        desc: "",
+        link: "https://templated.co/"
+    
+    },
+    {
+        id: 15,
+        category: "Template", 
+        title: "templatemo",
+        desc: "",
+        link: "https://templatemo.com/"
+    
+    },
+    {
+        id: 16,
+        category: "Template", 
+        title: "tooplate",
+        desc: "",
+        link: "https://www.tooplate.com/"
+    
+    },
+    {
+        id: 17,
+        category: "Template", 
+        title: "UICOOKIES",
+        desc: "",
+        link: "https://uicookies.com/"
+    
+    },
+    {
+        id: 18,
+        category: "template", 
+        title: "W3Layouts",
+        desc: "",
+        link: "https://w3layouts.com/"
+    
     }
+
+
+
+
+
+
 ]
